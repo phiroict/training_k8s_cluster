27c070d214769eba8bed7a17910de91c5cc01f70
Philip Rodrigues <phiroict@users.noreply.github.com>
2023-01-22 22:22:59 +1300

    Extract network data from the nodes.

--- set-hosts.sh
+++ set-hosts.sh
@@ -1,4 +1,4 @@
 KUBE_SYSTEMS=$(prlctl list | grep kubernetes_kube | awk '{print $4}')
 for KUBE_SYSTEM in $KUBE_SYSTEMS; do
-   echo $KUBE_SYSTEM
+   echo $(prlctl exec "${KUBE_SYSTEM}" "ip a" | grep eth1 | awk 'BEGIN{FS=" "} {print $(NF-5)}') | awk '{print $2}'
 done
